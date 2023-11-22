af726a261ac357b09eaf14c80a7b3b445bdfe5d1
Alexey Shimanskii <ShimanskiiAlexey@yandex.ru>
2023-11-22 16:53:48 +0400

    Выполнение задания к спринту 3.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterValue: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    var count = 0
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        counterValue.text = "Значение счетчика: \(count)"
+        counterValue.numberOfLines = 0
+        
+    }
+    
 }
 
